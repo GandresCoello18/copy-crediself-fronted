b68d8705a2e2dbd41832428ac2a994b3368eee4b
GandresCoello18 <goyeselcoca@gmail.com>
2021-10-05 13:13:31 -0500

    fix index domain production config

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,16 +6,16 @@ export const BASE_API_IMAGES_CLOUDINNARY =
 export const BASE_API_IMAGES_CLOUDINNARY_SCALE =
   'https://res.cloudinary.com/cici/image/upload/c_scale,w_230/v1619715476';
 
-export const BASE_API_FILE_DOCUMENT = 'http://localhost:9000/static';
-
 export const BASE_FRONTEND_DEV = 'http://localhost:3000';
-export const BASE_FRONTEND_PROD = 'https://crediself-api.herokuapp.com';
+export const BASE_FRONTEND_PROD = 'https://copy-crediself-fronted.vercel.app';
 export const BASE_FRONTEND = BASE_FRONTEND_PROD;
 
-const apiDev = 'http://localhost:9000';
-// const apiProd = '';
+// const apiDev = 'http://localhost:9000';
+const apiProd = 'https://crediself-api.herokuapp.com';
 
-export const BASE_API = apiDev;
+export const BASE_API_FILE_DOCUMENT = `${apiProd}/static`;
+
+export const BASE_API = apiProd;
 
 export const api = axios.default.create({
   baseURL: `${BASE_API}/api`,

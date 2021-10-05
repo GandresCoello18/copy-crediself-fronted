07e33f4a5f7058d9a162bfe338039028c1f19952
GandresCoello18 <goyeselcoca@gmail.com>
2021-10-05 12:35:49 -0500

    replace domain production app

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,8 +9,8 @@ export const BASE_API_IMAGES_CLOUDINNARY_SCALE =
 export const BASE_API_FILE_DOCUMENT = 'http://localhost:9000/static';
 
 export const BASE_FRONTEND_DEV = 'http://localhost:3000';
-export const BASE_FRONTEND_PROD = '';
-export const BASE_FRONTEND = BASE_FRONTEND_DEV;
+export const BASE_FRONTEND_PROD = 'https://crediself-api.herokuapp.com';
+export const BASE_FRONTEND = BASE_FRONTEND_PROD;
 
 const apiDev = 'http://localhost:9000';
 // const apiProd = '';
